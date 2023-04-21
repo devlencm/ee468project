1f471d3dbef17261cd0c54f8cbed4fed5d85e6f2
bonkka7 <112648035+bonkka7@users.noreply.github.com>
2023-04-21 14:44:00 -0400

    Add files via upload
    Added transition page to integrate feature 5 easily

---
+++ ee468project-f4/ee468proj/ee468proj/urls.py
@@ -0,0 +1,22 @@
+#urls.py
+from django.contrib import admin  
+from django.urls import path, include
+
+from ee468proj.views import salary, dept_name, admin1, minumumTable, maximumTable, averageTable, name, home, instructor, student, instrr, instr
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home, name='home'),
+    path('instructor/', instructor, name='instructor'),
+    path('student/', student, name='student'),
+    path('admin1/', admin1, name='admin1'),
+    path('name/', name, name='name'),
+    path('salary/', salary, name='name'),
+    path('dept_name/', dept_name, name='dept_name'),
+    path('minumumTable/', minumumTable, name='minumumTable'),
+    path('maximumTable/', maximumTable, name='maximumTable'),
+    path('averageTable/', averageTable, name='averageTable'),
+    path('instrr/',instrr,name='instrr'),
+    path('instr/',instr, name='instr'),
+    ]
